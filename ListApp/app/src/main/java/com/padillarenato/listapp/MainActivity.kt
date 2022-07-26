bfeecb33d3e88c02bfe89e2c7ab26950b9336286
Renato Padilla <renato.padilla@epn.edu.ec>
2022-07-26 12:51:08 -0500

    ListApp 4

---
+++ ListApp/app/src/main/java/com/padillarenato/listapp/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.padillarenato.listapp
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+
+class MainActivity : AppCompatActivity() {
+    lateinit var listViewSitiosWeb: ListView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val listaSitiosWeb = arrayOf(
+            "https://www.epn.edu.ec",
+            "https://www.fis.epn.edu.ec",
+            "https://www.google.com",
+            "https://www.microsoft.com"
+        )
+        listViewSitiosWeb = findViewById(R.id.listViewSitiosWeb)
+        listViewSitiosWeb.adapter = ArrayAdapter(this,R.layout.simple_list_item_p,resources.getStringArray(R.array.array_websites))
+        listViewSitiosWeb.setOnItemClickListener { parent, view, position, id ->
+            val intencion = Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com"))
+            startActivity(intencion)
+        }
+    }
+}
